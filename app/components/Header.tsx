98108c427165562bb08d9019f467ad2a451a3175
RozalenokM <deathwing00000@gmail.com>
2024-06-12 18:26:47 +0700

    feat: header changed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,40 @@
 import Link from "next/link";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "../styles/Home.module.css";
+import {
+  AppBar,
+  Box,
+  Button,
+  Icon,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 const Header = () => (
-  <nav>
-    <div className="flex flex-row items-center justify-between">
-      <Link href="/">Home</Link>
-      <ConnectButton />
-    </div>
-  </nav>
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <Button href="/" sx={{ my: 2, color: "white", display: "block" }}>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              More Protocol
+            </Typography>
+          </Button>
+        </Box>
+        {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link href="/">Home</Link>
+        </Typography> */}
+        <ConnectButton />
+      </Toolbar>
+    </AppBar>
+  </Box>
+  // <nav>
+  //   <div className="flex flex-row items-center justify-between">
+  //     <Link href="/">Home</Link>
+  //     <ConnectButton />
+  //   </div>
+  // </nav>
 );
 
 export default Header;
